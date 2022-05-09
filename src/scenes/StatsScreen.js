8e51429d488b9583f557f326243361898ffce550
Aaron-Vuong <81384460+Aaron-Vuong@users.noreply.github.com>
2022-05-09 11:42:02 -0700

    Moved button logic into one call.

--- src/scenes/StatsScreen.js
+++ src/scenes/StatsScreen.js
@@ -18,10 +18,10 @@ class StatsScreen extends Phaser.Scene {
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding - 30, "Stats Screen", menuConfig).setOrigin(0.5);
         this.testX = this.add.text(game.config.width/2, game.config.height/2 + 100, "Starting stuff: " + game.settings.stat1, menuConfig).setOrigin(0.5);
 
-        this.button = new Button(80, 30, 'Start Game', config, this, () => game.settings.stat1 += 1);
+        this.button = new Button(80, 30, 'Start Game', config, this, 
+                                () => this.testX.text = "Starting stuff: " + (game.settings.stat1 += 1));
 
     }
     update() {
-        this.testX.text = "Starting stuff: " + game.settings.stat1;
     }
 }
