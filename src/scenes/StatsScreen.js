f2740c33bc5a4d80ec5caed30345d0889ff2a83b
Margarita Rodriguez <mrodr157@ucsc.edu>
2022-05-11 20:11:10 -0700

    added first text input

--- src/scenes/StatsScreen.js
+++ src/scenes/StatsScreen.js
@@ -16,20 +16,27 @@ class StatsScreen extends Phaser.Scene {
         }
 
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding - 150, "Stats Screen", menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, "Strength", menuConfig).setOrigin(0.5);
+
         var textEntry = this.add.text(game.config.width/2, game.config.height/2, " " + game.settings.stat1, menuConfig).setOrigin(0.5);
 
         this.input.keyboard.on('keydown', function(event) {
             if(event.keyCode === 8 && textEntry.text.length > 0) {
-                textEntry.text = textEntry.text.subtr(0, textEntry.text.length - 1);
+                textEntry.text = textEntry.text.substr(0, textEntry.text.length - 1);
             } 
             else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 90)) {
                 textEntry.text += event.key;
             }
         });
 
+        var int = parseInt(textEntry);
+
         this.button = new Button(80, 30, 'Start Game', config, this, 
-                                () => this.textEntry.text = "Strength: " + (game.settings.stat1 += 1));
-    }
+                                () => this.textEntry.text = "Strength: " + (game.settings.stat1 += this.int));
+        console.log(game.settings.stat1);
+        }
+
+        //so far 30 mins on stat input
 
     update() {
     }
