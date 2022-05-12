eab65836c25321a6cb76287d3c5bec140d4d56f2
Denae <dfickes@ucsc.edu>
2022-05-11 22:04:01 -0700

    Added Menu Animation

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,7 +2,19 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+    preload() {
+        this.load.spritesheet('FoldOut', './assets/MenuAnims.png', {frameWidth: 1000, frameHeight: 500, startFrame: 0, endFrame: 44});
+        
+    }
+
     create() {
+        this.scene.anims.create({
+            key: 'FoldOut',
+            frames: this.anims.generateFrameNumbers('FoldOut', {start: 0, end: 44, first: 0}),
+            frameRate: 30
+        });
+        this.anims.play('FoldOut')
+
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '36px',
