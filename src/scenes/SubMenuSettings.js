0d0379b08e390825f489b3cc18dca6363a1d39e7
Denae <dfickes@ucsc.edu>
2022-06-01 11:27:31 -0700

    Instructions for controls in settings

--- src/scenes/SubMenuSettings.js
+++ src/scenes/SubMenuSettings.js
@@ -11,12 +11,35 @@ class Settings extends Phaser.Scene {
         this.booton.on("pointerdown", () => {
             this.scene.stop();
             this.scene.run(game.settings.prevScene);
+
+            //Load() {
+                //let settingsConfig = {
+                   // fontFamily: 'Consolas',
+                    //fontSize: '36px',
+                    //color: '#85c7b1',
+                    //align: 'center',
+                    //stroke: '#000000',
+                    //strokeThickness: 6,
+                    //padding: {
+                        //top: 5,
+                       // bottom: 5,
+                    //},
+                    //fixedWidth: 0
+               // }
+                //menu text
+                
+            
         });
 
         this.settingsRectangle = this.add.rectangle(game.config.width/2, game.config.height/2, 500, 400, 0x6666ff).setInteractive();
+
+        this.settings1 = this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding+100, "Use WASD to move or UPDOWNLEFTRIGHT").setOrigin(0.5);
+        this.settings2 = this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding+130, "F to Interact").setOrigin(0.5);
+        this.settings1.scale= 1.3;
+        this.settings2.scale= 1.3;
         
         keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
-        
+
     }
 
     update() {
